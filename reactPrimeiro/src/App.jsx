b775c37f01784d7f7a73d98db532fa3515e5c0f2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-22 11:50:27 -0300

    Adicionando Aula01

---
+++ reactPrimeiro/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Banner from './componentes/Banner/Banner'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Teste</h1>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ reactPrimeiro/src/componentes/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+function Banner(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Banner
